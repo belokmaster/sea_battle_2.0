9bf532325ceead667960d97f9fcdb22ef26f6b55
belokmaster <sychev.nickolay1@gmail.com>
2025-08-25 14:50:21 +0300

    game is done

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 type Point struct {
@@ -18,37 +19,207 @@ type Ship struct {
 	Position   []Point
 }
 
+type CellState int
+
+const (
+	EmptyCell CellState = iota
+	ShipCell
+	MissCell
+	HitCell
+)
+
 type Board struct {
-	Grid  [10][10]int
+	Grid  [10][10]CellState
 	Ships []Ship
+}
+
+type AttackResult int
+
+const (
+	ResultMiss AttackResult = iota
+	ResultHit
+	ResultSunk
+)
+
+type Player struct {
+	Name       string
+	MyBoard    *Board
+	EnemyBoard *Board
+}
+
+type Game struct {
+	Player1       *Player
+	Player2       *Player
+	CurrentPlayer *Player
 }
 
 func newBoard() *Board {
 	return &Board{
-		Grid:  [10][10]int{},
+		Grid:  [10][10]CellState{},
 		Ships: []Ship{},
 	}
 }
 
-func main() {
-	gameBoard := newBoard()
+func NewGame() *Game {
+	board1 := newBoard()
+	board2 := newBoard()
+	board1.PlaceBoard()
+	board2.PlaceBoard()
 
-	ship1 := Ship{Size: 3, IsVertical: false}
-	if err := gameBoard.placeShip(&ship1, Point{X: 0, Y: 2}); err != nil {
-		fmt.Println("Ошибка:", err)
-	}
-	gameBoard.printField()
-
-	ship2 := Ship{Size: 2, IsVertical: true}
-	fmt.Println("Ставим корабль (2, вертикальный) в точку B1 (x:1, y:1), вплотную к первому. Должна быть ошибка.")
-	if err := gameBoard.placeShip(&ship2, Point{X: 0, Y: 1}); err != nil {
-		fmt.Println("Поймали ожидаемую ошибку:", err)
+	p1 := Player{
+		Name:       "Player",
+		MyBoard:    board1,
+		EnemyBoard: board2,
 	}
 
-	gameBoard.printField()
+	p2 := Player{
+		Name:       "Computer",
+		MyBoard:    board2,
+		EnemyBoard: board1,
+	}
+
+	game := Game{
+		Player1:       &p1,
+		Player2:       &p2,
+		CurrentPlayer: &p1,
+	}
+
+	return &game
 }
 
-func (b *Board) printField() {
+func (g *Game) StartGame() {
+CurrentGameLoop:
+	for {
+		fmt.Printf("Ход игрока: %s\n", g.CurrentPlayer.Name)
+		if g.CurrentPlayer.Name == "Player" {
+			fmt.Println("Ваше поле:")
+			g.Player1.MyBoard.printField(false)
+
+			fmt.Println("Поле противника:")
+			g.Player1.EnemyBoard.printField(true)
+		}
+
+		var result AttackResult
+		var err error
+
+		if g.CurrentPlayer == g.Player1 {
+			fmt.Print("Введите координаты для атаки: ")
+			var x, y int
+			fmt.Scan(&x, &y)
+			attackPoint := Point{X: x, Y: y}
+			result, err = g.CurrentPlayer.EnemyBoard.Attack(&attackPoint)
+			if err != nil {
+				fmt.Println("Ошибка:", err)
+			}
+
+			if g.CurrentPlayer.EnemyBoard.AllShipSunk() {
+				fmt.Printf("Победил игрок: %s\n", g.CurrentPlayer.Name)
+				break CurrentGameLoop
+			}
+		} else {
+			_, _ = g.CurrentPlayer.EnemyBoard.AttackBot()
+			if g.CurrentPlayer.EnemyBoard.AllShipSunk() {
+				fmt.Print("Победил бот", g.CurrentPlayer.Name)
+				break CurrentGameLoop
+			}
+		}
+
+		if g.CurrentPlayer.EnemyBoard.AllShipSunk() {
+			fmt.Printf("Победил игрок: %s\n", g.CurrentPlayer.Name)
+			break CurrentGameLoop
+		}
+
+		if result == ResultMiss {
+			fmt.Println("Промах! Ход переходит.")
+			if g.CurrentPlayer == g.Player1 {
+				g.CurrentPlayer = g.Player2
+			} else {
+				g.CurrentPlayer = g.Player1
+			}
+		} else {
+			fmt.Println("Попадание! Вы ходите еще раз.")
+		}
+	}
+}
+
+func (b *Board) AttackBot() (AttackResult, error) {
+	var currentPoint Point
+	for {
+		x := rand.Intn(10)
+		y := rand.Intn(10)
+		if b.Grid[x][y] != MissCell && b.Grid[x][y] != HitCell {
+			currentPoint = Point{X: x, Y: y}
+			fmt.Printf("Бот атакует клетку: %d %d\n", currentPoint.X, currentPoint.Y)
+			break
+		}
+	}
+	return b.Attack(&currentPoint)
+}
+
+func (b *Board) PlaceBoard() {
+	possibleShips := [10]int{4, 3, 3, 2, 2, 2, 1, 1, 1, 1}
+	for _, size := range possibleShips {
+		for {
+			ship := Ship{
+				Size:       size,
+				IsVertical: (rand.Intn(2) == 1),
+			}
+
+			x := rand.Intn(10)
+			y := rand.Intn(10)
+			startPoint := Point{X: x, Y: y}
+			if b.placeShip(&ship, startPoint) == nil {
+				break
+			}
+		}
+	}
+	b.printField(false)
+}
+
+func (b *Board) AllShipSunk() bool {
+	for i := range b.Ships {
+		if !b.Ships[i].IsSunk {
+			return false
+		}
+	}
+	return true
+}
+
+func (b *Board) Attack(p *Point) (AttackResult, error) {
+	if p.X < 0 || p.X >= 10 || p.Y < 0 || p.Y >= 10 {
+		return ResultMiss, errors.New("атака вне поля")
+	}
+
+	currentSquare := b.Grid[p.X][p.Y]
+	if currentSquare == MissCell || currentSquare == HitCell {
+		return ResultMiss, errors.New("по этой клетке уже стреляли")
+	} else if currentSquare == EmptyCell {
+		b.Grid[p.X][p.Y] = MissCell
+		return ResultMiss, nil
+	} else {
+		b.Grid[p.X][p.Y] = HitCell
+		for i := range b.Ships {
+			ship := &b.Ships[i]
+			for j := 0; j < ship.Size; j++ {
+				x, y := ship.Position[j].X, ship.Position[j].Y
+				if p.X == x && p.Y == y {
+					fmt.Printf("Удар по клетке %d %d нанесен\n", p.X, p.Y)
+					ship.Hits++
+
+					if ship.Hits == ship.Size {
+						fmt.Println("Корабль был разрушен")
+						ship.IsSunk = true
+						return ResultSunk, nil
+					}
+					return ResultHit, nil
+				}
+			}
+		}
+		return ResultMiss, errors.New("ошибка состояния: клетка корабля есть, а самого корабля нет")
+	}
+}
+
+func (b *Board) printField(isEnemyView bool) {
 	fmt.Println()
 	fmt.Print("    ")
 	for c := 0; c < 10; c++ {
@@ -57,13 +228,27 @@ func (b *Board) printField() {
 	fmt.Println()
 
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%2d  ", i+1)
+		fmt.Printf("%2d  ", i)
 		for j := 0; j < 10; j++ {
-			if b.Grid[i][j] == 1 {
-				fmt.Print("■  ")
-			} else {
-				fmt.Print(".  ")
+			cell := b.Grid[i][j]
+			char := ""
+			switch cell {
+			case HitCell:
+				char = "X  "
+			case MissCell:
+				char = "o  "
+			case EmptyCell:
+				char = ".  "
+			case ShipCell:
+				if isEnemyView {
+					char = ".  "
+				} else {
+					char = "■  "
+				}
+			default:
+				char = "?  "
 			}
+			fmt.Print(char)
 		}
 		fmt.Println()
 	}
@@ -89,7 +274,7 @@ func (b *Board) placeShip(ship *Ship, startPoint Point) error {
 			for dy := -1; dy <= 1; dy++ {
 				checkX, checkY := p.X+dx, p.Y+dy
 				if checkX >= 0 && checkX < 10 && checkY >= 0 && checkY < 10 {
-					if b.Grid[checkX][checkY] == 1 {
+					if b.Grid[checkX][checkY] == ShipCell {
 						return errors.New("корабль соприкасается или пересекается с другим")
 					}
 				}
@@ -98,11 +283,16 @@ func (b *Board) placeShip(ship *Ship, startPoint Point) error {
 	}
 
 	for _, p := range shipPoints {
-		b.Grid[p.X][p.Y] = 1
+		b.Grid[p.X][p.Y] = ShipCell
 	}
 
 	ship.Position = shipPoints
 	b.Ships = append(b.Ships, *ship)
 
 	return nil
+}
+
+func main() {
+	game := NewGame()
+	game.StartGame()
 }
